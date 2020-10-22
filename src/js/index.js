55d07433aded56c6fa1e442cfdee81ef156716ea
Talon Durrant <talonmdurrant@gmail.com>
2020-10-21 22:59:18 -0600

    adding in husky and lint staged

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,4 @@
-import _ from "lodash"
-import "../main.scss"
+import _ from 'lodash';
+import '../main.scss';
 
-console.log(_.join(["Index", "module", "loaded!"], " "))
-
-const paragraph = document.createElement("p")
-paragraph.textContent = "I am injected from the JavaScript file."
-
-document.querySelector("body").appendChild(paragraph)
+console.log(_.join(['Index', 'module', 'loaded!'], ' '));
